91fc5033593bd9b50b5357336727e9c10df4eb51
vmedeir0s <vmeedeiros@gmail.com>
2023-04-25 18:12:25 -0300

    feat: implemented features

--- src/main.js
+++ src/main.js
@@ -1,1 +1,39 @@
 import './style.css';
+import Swal from 'sweetalert2';
+
+const inputEl = document.querySelector('input');
+const btnEl = document.querySelector('button');
+const preEl = document.querySelector('pre');
+
+inputEl.addEventListener('keypress', () => {
+  const inputLength = inputEl.value.length;
+
+  if (inputLength === 5) {
+    inputEl.value += '-';
+  }
+});
+
+btnEl.addEventListener('click', async () => {
+  const cep = inputEl.value;
+
+  if (cep.length === 0) {
+    Swal.fire({
+      icon: 'error',
+      text: 'Digite um CEP',
+    });
+    return null;
+  }
+
+  try {
+    const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await result.json();
+    preEl.innerHTML = JSON.stringify(data, undefined, 1);
+    return data;
+  } catch (error) {
+    Swal.fire({
+      icon: 'error',
+      text: error.message,
+    });
+    return null;
+  }
+});
